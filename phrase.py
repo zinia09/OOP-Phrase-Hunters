0f1b6cfb8d2a90da51c5ef53bb4d3d29d2e12974
zinia09 <110440881+zinia09@users.noreply.github.com>
2023-01-14 23:10:30 -0500

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,15 +1,24 @@
-
-class Phrase():
-    pass
+class Phrase:
 
     def __init__(self, phrase):
         self.phrase = phrase.lower()
 
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                print(f"{letter}", end=" ")
+            else:
+                print("_", end=" ")
 
+    def check_guess(self, guess):
+        if guess in self.phrase:
+            return True
+        else:
+            return False
 
-
-
-
-
-
-
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if letter not in guesses:
+                return False
+            # else:
+        return True
