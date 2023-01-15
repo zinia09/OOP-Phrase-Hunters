94c333ff92d7cac5affbf1c4441ebeeec9c873be
zinia09 <110440881+zinia09@users.noreply.github.com>
2023-01-14 23:09:30 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 from phrase import Phrase
 import random
 
+
 class Game:
 
     def __init__(self):
         self.missed = 0
         self.phrases = []
-        self.active_phrase = None
         self.guesses = [" "]
+        self.phrases = self.create_phrases()
+        self.active_phrase = self.get_random_phrase()
 
     def create_phrases(self):
         phrases = []
@@ -18,29 +20,29 @@ class Game:
         phrases.append(Phrase("Have a good day"))
         return phrases
 
-
     def get_random_phrase(self):
         random_phrase = random.choice(self.phrases)
         return random_phrase
 
-#
-#     def welcome(self):
-#         print("============================\n  Welcome to Phrase Hunter\n============================")
-#         print(f"“Number missed: {self.missed}")
-#         display(active_phrase)
-#
+    def welcome(self):
+        print("============================\n  Welcome to Phrase Hunter\n============================")
 
-#
-#
-#     def get_guess():
-#         get_guess = input(Enter a letter:)
-#         return get_guess
-#
-#     def start():
-#         append
-#         check_guess(active_phrase)
-#         if self.active_phrase.check_guess(user_guess):
-#             print("YAY")
-#         else:
-#             print("Bummer!")
+    def start(self):
+        self.welcome()
+        while self.missed < 5 and not self.active_phrase.check_complete(self.guesses):
+            print(f"\nNumber missed: {self.missed}")
+            self.active_phrase.display(self.guesses)
+            user_guess = self.get_guess()
+            self.guesses.append(user_guess)
+            if not self.active_phrase.check_guess(user_guess):
+                self.missed += 1
+        self.game_over()
 
+    def get_guess(self):
+        return input("\nEnter a letter: ")
+
+    def game_over(self):
+        if self.missed >= 5:
+            print("Sorry you lost")
+        else:
+            print("Congratulations! You Won!")
